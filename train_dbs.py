d4ec447b789f6cdca0ec1753215bf870bb489ca3
MydasTouch <159111376+MydasTouch@users.noreply.github.com>
2024-02-12 23:31:42 +0900

    Update train_dbs.py

--- train_dbs.py
+++ train_dbs.py
@@ -9,19 +9,18 @@ from transformers import HubertModel
 from transformers import AutoFeatureExtractor, WhisperModel
 
 from model import AssessGradeForDBS
-from dataset import MyDataset
+from dataset import DBSDataset
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=str, required=False,default ="data", help='Path of your DATA/ directory')
-    parser.add_argument('--finetune_from_checkpoint', type=str, required=False, help='Path to your checkpoint to finetune from')
-    parser.add_argument('--outdir', type=str, required=False, default='checkpoints/tuika_vowel_new/whsiper', help='Output directory for your trained checkpoints')
+    parser.add_argument('--outdir', type=str, required=False, default='checkpoints/dbs', help='Output directory for your trained checkpoints')
     args = parser.parse_args()
 
     datadir = args.datadir
     ckptdir = args.outdir
-    my_checkpoint = args.finetune_from_checkpoint
+    my_checkpoint = args.checkpoint
     
     if not os.path.exists(ckptdir):
         os.system('mkdir -p ' + ckptdir)
@@ -29,145 +28,63 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('DEVICE: ' + str(device))
 
-    trainlist = os.path.join(datadir, 'sets/ready_to_use_tuika_16k_old/train.csv')
-    validlist = os.path.join(datadir, 'sets/ready_to_use_tuika_16k_old/dev.csv')
-
-    # ssl_model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
-    # ssl_model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
-    # ssl_model = HubertModel.from_pretrained("facebook/hubert-base-ls960")
-    # ssl_model = HubertModel.from_pretrained("rinna/japanese-hubert-base")
-    # for param in ssl_model.base_model.parameters():
-    #     param.requires_grad = False
-    SSL_OUT_DIM = 768
-    GRBAS_DIM = 1 # multi-task
+    # ssl module
     ssl_model = HubertModel.from_pretrained("rinna/japanese-hubert-base")
+
+    # asr module
     asr_model = WhisperModel.from_pretrained("openai/whisper-small",output_attentions=True)
-    # model.config.decoder_start_token_id
     feature_extractor = AutoFeatureExtractor.from_pretrained("openai/whisper-small")
     decoder_input_ids = torch.tensor([[1, 1]]) * asr_model.config.decoder_start_token_id
     decoder_input_ids = decoder_input_ids.to(device)
+    
+    trainlist = os.path.join(datadir, 'sets/ready_to_use_tuika_16k_old/train.csv')
+    validlist = os.path.join(datadir, 'sets/ready_to_use_tuika_16k_old/dev.csv')
 
-    trainset = MyDataset(trainlist,feature_extractor)
+    ssl_out_dim = 768
+    grbas_dim = 3 # 1: regression  3: classification
+    multi_indicator = False
+
+    trainset = DBSDataset(trainlist,feature_extractor,multi_indicator)
     trainloader = DataLoader(trainset, batch_size=1, shuffle=False, num_workers=1, collate_fn=trainset.collate_fn)
 
-    validset = MyDataset(validlist,feature_extractor)
+    validset = DBSDataset(validlist,feature_extractor,multi_indicator)
     validloader = DataLoader(validset, batch_size=1, shuffle=False, num_workers=1, collate_fn=validset.collate_fn)
 
-    net = MosPredictor(ssl_model,asr_model,decoder_input_ids, SSL_OUT_DIM, GRBAS_DIM)
+    net = GRBASPredictor(ssl_model,asr_model,decoder_input_ids, ssl_out_dim, grbas_dim)
     net = net.to(device)
-    # for param in net.parameters():
-    #     print(param.requires_grad)
-    if my_checkpoint != None:  ## do (further) finetuning
-        net.load_state_dict(torch.load(my_checkpoint))
     
-    # criterion = nn.L1Loss()
-    # weights = [131/83,1,131/88]
-    # class_weights = torch.FloatTensor(weights).to(device)
-    criterion = nn.CrossEntropyLoss(label_smoothing=0.1)
-    downsample = nn.Sequential(
-            nn.MaxPool1d(400,320)
-            # nn.MaxPool1d(10,5),
-            # nn.MaxPool1d(3,2),
-            # nn.MaxPool1d(3,2),
-            # nn.MaxPool1d(3,2),
-            # nn.MaxPool1d(3,2),
-            # nn.MaxPool1d(2,2),
-            # nn.MaxPool1d(2,2)
-        )
-    # def criterion(est,target):
-    #     # print(est)
-    #     # print(target)
-    #     est = torch.nn.functional.softmax(est,dim=-1)
-    #     # print(est)
-    #     est = est.squeeze()
-    #     target = target.squeeze()
-    #     onehot_out = nn.functional.one_hot(target,num_classes=3)
-    #     position = torch.argmax(onehot_out)
-    #     p = 0
-    #     loss = 0
-    #     for i,j in zip(est,onehot_out):
-    #         cross_loss = -torch.log(1-i)
-    #         position_loss = torch.pow(p-position,2)
-    #         p = p+1
-    #         loss += (cross_loss*position_loss)
-    #     # print(loss)
-    #     # if est_postion == 0:
-    #     #     weight = 3/0.71
-    #     # elif est_postion ==1 :
-    #     #     weight = 3/1.44
-    #     # else:
-    #     #     weight = 3/0.85
-        
-    #     # print(est)
-    #     # print(target)
-    #     # print(onehot_out)
-    #     # print(position)
-    #     # print(est_postion,position)
-    #     # index = torch.tensor([0,1,2]).to(device =device)
-    #     # cross_loss = -torch.log(est[position])
-    #     # position_loss = torch.pow(est_postion-position,2)
-    #     # print(torch.pow(cross_loss,position_loss))
-    #     # print(cross_loss * position_loss)
-    #     return loss
     def criterion(est,target):
-        # print(est)
-        # print(target)
         est = est.squeeze()
         target = target.squeeze()
         onehot_out = nn.functional.one_hot(target,num_classes=3)
         est_postion = torch.argmax(est)
-        # if est_postion == 0:
-        #     weight = 3/0.71
-        # elif est_postion ==1 :
-        #     weight = 3/1.44
-        # else:
-        #     weight = 3/0.85
         position = torch.argmax(onehot_out)
-        # print(est)
-        # print(target)
-        # print(onehot_out)
-        # print(position)
-        # print(est_postion,position)
-        # index = torch.tensor([0,1,2]).to(device =device)
+
         cross_loss = -torch.log(est[position])
         position_loss = torch.abs(est_postion-position)
-        # print(torch.pow(cross_loss,position_loss))
-        # print(cross_loss * position_loss)
         return cross_loss * position_loss
     optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
-    # optimizer = optim.SGD(net.parameters(), lr=0.0005, momentum=0.8)
-    # optimizer = optim.AdamW(net.parameters(), lr=0.001)
 
-    PREV_VAL_LOSS=999999
+    PREV_VAL_LOSS=999
     orig_patience=20
     patience=orig_patience
-    for epoch in range(1,1001):
+    for epoch in range(1,200):
         STEPS=0
         net.train()
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             asr_mel_features, inputs,mel_specgrams, labels, filenames = data
-            # mel_specgram = mel_specgram.to(device)
+
             asr_mel_features = asr_mel_features.to(device)
             inputs = inputs.to(device)
             mel_specgrams = mel_specgrams.to(device)
             labels = labels.to(device)
-            # mask = mask.to(device)
-            # target_masks = downsample(mask).squeeze(1)
-            # B,T = target_masks.size()
-            # labels = labels.long().squeeze(1)
-            # print(labels)
             labels = torch.ceil(labels).long().squeeze(1)-1
-            # print(labels)
-            # labels = labels * target_masks
-            # Non_zero = torch.count_nonzero(target_masks,dim=1)
 
             optimizer.zero_grad()
             outputs = net(asr_mel_features, inputs,mel_specgrams).squeeze(1)
-            # print(outputs.size())
-            # print(labels.size())
+
             loss = criterion(outputs, labels)
-            # loss = loss/Non_zero
             loss.backward()
             optimizer.step()
             STEPS += 1
@@ -176,7 +93,7 @@ def main():
         print('AVG EPOCH TRAIN LOSS: ' + str(running_loss / STEPS))
         epoch_val_loss = 0.0
         net.eval()
-        ## clear memory to avoid OOM
+
         with torch.cuda.device(device):
             torch.cuda.empty_cache()
         ## validation
@@ -185,25 +102,16 @@ def main():
             VALSTEPS+=1
 
             asr_mel_features, inputs,mel_specgrams, labels, filenames = data
-            # mel_specgram = mel_specgram.to(device)
             asr_mel_features = asr_mel_features.to(device)
             inputs = inputs.to(device)
             mel_specgrams = mel_specgrams.to(device)
             labels = labels.to(device)
-            # mask = mask.to(device)
-            # target_masks = downsample(mask).squeeze(1)
-            # print(labels)
-            # labels = labels.long().squeeze(1)
-            # labels = torch.ceil(labels).long()-1
             labels = torch.ceil(labels).long().squeeze(1)-1
-            # print(labels)
+
             outputs = net(asr_mel_features, inputs,mel_specgrams).squeeze(1)
-            # print()
+
             loss = criterion(outputs, labels)
             epoch_val_loss += loss.item()
-            # if i ==40:
-            #     print(labels)
-            #     print(outputs)
         avg_val_loss=epoch_val_loss/VALSTEPS    
         print('EPOCH VAL LOSS: ' + str(avg_val_loss))
         if avg_val_loss < PREV_VAL_LOSS:
